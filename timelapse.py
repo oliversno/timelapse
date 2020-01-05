c24bbee124ee849676af65a8e443b3503defd0dc
Sean Oliver <snoliver@umich.edu>
2020-01-04 21:04:45 -0500

    modified base to use custom directory

--- timelapse.py
+++ timelapse.py
@@ -1,1 +1,49 @@
 #!/usr/local/bin/python3
+
+import cv2 as cv
+import argparse
+import os
+
+# Construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-dir", "--directory", required=False, default='.', help="image directory. default is current directory")
+ap.add_argument("-ext", "--extension", required=False, default='png', help="extension name. default is 'png'.")
+ap.add_argument("-o", "--output", required=False, default='output.mp4', help="output video file")
+args = vars(ap.parse_args())
+
+# Arguments
+dir_path = args['directory']
+ext = args['extension']
+output = args['output']
+
+images = []
+for f in os.listdir(dir_path):
+    if f.endswith(ext):
+        images.append(f)
+
+# Determine the width and height from the first image
+image_path = os.path.join(dir_path, images[0])
+frame = cv.imread(image_path)
+cv.imshow('video',frame)
+height, width, channels = frame.shape
+
+# Define the codec and create VideoWriter object
+fourcc = cv.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
+out = cv.VideoWriter(output, fourcc, 20.0, (width, height))
+
+for image in images:
+
+    image_path = os.path.join(dir_path, image)
+    frame = cv.imread(image_path)
+
+    out.write(frame) # Write out frame to video
+
+    cv.imshow('video',frame)
+    if (cv.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
+        break
+
+# Release everything if job is finished
+out.release()
+cv.destroyAllWindows()
+
+print("The output video is {}".format(output))
