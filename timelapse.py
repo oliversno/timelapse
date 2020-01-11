04c10cbb3c85f5e750a9c37bbbc707ef83508323
Sean Oliver <snoliver@umich.edu>
2020-01-11 18:45:41 -0500

    added time percent functionality

--- timelapse.py
+++ timelapse.py
@@ -3,12 +3,15 @@
 import cv2 as cv
 import argparse
 import os
+import time
 
 # Construct the argument parser and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-dir", "--directory", required=False, default='.', help="image directory. default is current directory")
 ap.add_argument("-ext", "--extension", required=False, default='png', help="extension name. default is 'png'.")
 ap.add_argument("-o", "--output", required=False, default='output.mp4', help="output video file")
+ap.add_argument("-t", "--time", required=True, help="")
+ap.add_argument("-et", "--ending_time", required=False, default="-1", help="")
 args = vars(ap.parse_args())
 
 # Arguments
@@ -21,7 +24,12 @@ for f in os.listdir(dir_path):
     if f.endswith(ext):
         images.append(f)
 images.sort(key=lambda x: os.path.getctime(os.path.join(dir_path, x)))
-print(images)
+end_time = os.path.getctime(os.path.join(dir_path, images[-1]))
+start_time = os.path.getctime(os.path.join(dir_path, images[0]))
+ending_time = int(args['ending_time'])
+if end_time < 0:
+    end_time = (end_time - start_time)/size(images)
+time_diff = end_time - start_time + ending_time
 
 # Determine the width and height from the first image
 image_path = os.path.join(dir_path, images[0])
@@ -32,10 +40,14 @@ height, width, channels = frame.shape
 # Define the codec and create VideoWriter object
 fourcc = cv.VideoWriter_fourcc(*'mp4v') # Be sure to use lower case
 out = cv.VideoWriter(output, fourcc, 20.0, (width, height))
-
-for image in images:
-
-    image_path = os.path.join(dir_path, image)
+cur_image = 0
+for timestep in range(int(float(args['time'])*60)-ending_time):
+    print(timestep)
+    percent = timestep/(float(args['time'])*60-ending_time)
+    date = start_time + time_diff*percent
+    if date >= os.path.getctime(os.path.join(dir_path, images[cur_image+1])):
+        cur_image += 1
+    image_path = os.path.join(dir_path, images[cur_image])
     frame = cv.imread(image_path)
 
     out.write(frame) # Write out frame to video
@@ -43,6 +55,7 @@ for image in images:
     cv.imshow('video',frame)
     if (cv.waitKey(1) & 0xFF) == ord('q'): # Hit `q` to exit
         break
+    time.sleep(1)
 
 # Release everything if job is finished
 out.release()
